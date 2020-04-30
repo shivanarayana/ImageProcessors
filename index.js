182c29d1e6d716203287634e50cabb48d6706097
venkata shiva narayana modini <shivanarayana@users.noreply.github.com>
2020-04-30 12:05:01 +0530

    Add files via upload
    index files javascript first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//simple console experiment
+let name='shiva narayana';
+const joindate= '29/04/2020';
+let arra = ['working','in','mavoix'];
+let address=['suncity','hyderabad','telangana'];
+console.log(name+" joined on:"+joindate+":"+arra+"lives in:"+address);
+
